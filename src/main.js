c24c4feca73842a51fb9aa87e31b7cc3176ac62a
R-Amoedo <126144724+R-Amoedo@users.noreply.github.com>
2023-12-16 01:57:23 -0300

    main do array

---
+++ src/main.js
@@ -0,0 +1,18 @@
+let alunos = [
+    {
+        'nome': 'Sara',
+        'nota': 10
+    },
+    {
+        'nome': 'Carlos',
+        'nota': 5
+    },
+    {
+        'nome': 'Larissa',
+        'nota': 7
+    },
+    {
+        'nome': 'Diogo',
+        'nota': 8
+    }
+];
